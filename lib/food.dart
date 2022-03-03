83d026045f1e82bc979a29198098ff40e827cc65
Bethel <Beamen480@yahoo.com>
2022-03-03 13:19:26 +0000

    Added somethings

--- lib/food.dart
+++ lib/food.dart
@@ -1,11 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:melatech/cart.dart';
+import 'package:melatech/options.dart';
+import 'package:melatech/veges.dart';
 
-class FoodPage extends StatelessWidget {
+class FoodPage extends StatefulWidget {
   const FoodPage({ Key? key }) : super(key: key);
 
   @override
+  State<FoodPage> createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+
+
+// ignore: unused_field
+int _currentIndex = 0;
+// ignore: unused_field
+final List<Widget> _children = [
+ const VegesPage(),
+ const CartPage(),
+ const OptionsPage(),
+];
+
+void _onItemTapped(int index){
+  setState((){
+    _currentIndex = index;
+  });
+}
+
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+  type: BottomNavigationBarType.fixed,
+  backgroundColor:const Color(0xFFffffff),
+  selectedItemColor: const Color(0xFF4e055a),
+  unselectedItemColor: Color(0xFF4e055a).withOpacity(.60),
+  selectedFontSize: 14,
+  unselectedFontSize: 14,
+  onTap: _onItemTapped,
+  items: const [
+    BottomNavigationBarItem(
+      label: 'Home',
+      icon: Icon(Icons.home,
+      color:Color(0xFF4e055a)
+      ),
+    ),
+    BottomNavigationBarItem(
+      label: 'Cart',
+      icon: Icon(Icons.shopping_cart_outlined,
+      color:Color(0xFF4e055a)
+      )
+    ),
+    BottomNavigationBarItem(
+      label: 'Options',
+      icon: Icon(Icons.more_vert,
+      color:Color(0xFF4e055a)
+      ),
+    ),
+  ],
+  currentIndex: _currentIndex,
+),
+    //   bottomNavigationBar: BottomAppBar(
+    //     elevation: 0,
+    //     color: Colors.white,
+    //     child: Row(
+    //   children: [
+    //     IconButton(icon: const Icon(Icons.home), 
+    //     color: const Color(0xFF4e055a),
+    //     onPressed: () {}),
+    //    const Spacer(),
+    //     IconButton(icon: const Icon(Icons.shopping_cart_outlined),
+    //     color: const Color(0xFF4e055a),
+    //      onPressed: () {}),
+    //    const  Spacer(),
+    //     IconButton(icon: const Icon(Icons.more_vert),
+    //     color: const Color(0xFF4e055a),
+    //      onPressed: () {}),
+    //   ],
+    // ),
+    //   ),
+      body: _children[_currentIndex],
       
     );
   }
---
+++ lib/options.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OptionsPage extends StatelessWidget {
+  const OptionsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+}
